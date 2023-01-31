04ecc1bfc2455f1145433af3f3096575c778c7dd
edmond buck <edmond.buck@wearetissue.com>
2023-01-31 09:27:39 -0800

    created function for renderlicenselink

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -18,7 +18,20 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'MIT') {
+    return 'https://github.com/git/git-scm.com/blob/main/MIT-LICENSE.txt';
+  }
+  if (license === 'Apache 2.0') {
+    return 'http://www.apache.org/licenses/LICENSE-2.0';
+  }
+  if (license === 'GNU GPL v3.0') {
+    return 'https://www.gnu.org/licenses';
+  }
+  if (license === 'None') {
+    return 'A license was not selected';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
